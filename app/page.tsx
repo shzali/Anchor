61975d3da90aea14ddc5dd5a4de7fdeaa64caf03
Shayan Ali <shayan677@gmail.com>
2026-08-21 10:41:07 +0100

    categories and tasks displayed as a list on mobile devices; can add tasks; can click on a task to change its status

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,40 @@ import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { ChevronRightIcon, ChevronLeftIcon } from "lucide-react"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectLabel,
+} from "@/components/ui/select"
 
 interface Task {
   id: string
   description: string
-  isComplete: boolean
+  status: "pending" | "complete" | "partially complete" | "incomplete"
 }
 
 interface Category {
@@ -21,7 +50,7 @@ const Home = () => {
   const [date, setDate] = useState(new Date())
   // 'id' holds the id of the category for which the task is being added
   const [newTaskInput, setNewTaskInput] = useState({ id: "", description: "" })
-  const [isAddingNewTask, setIsAddingNewTask] = useState(false)
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
 
   const [planner, setPlanner] = useState<Category[]>([
     {
@@ -31,12 +60,12 @@ const Home = () => {
         {
           id: "1",
           description: "30 mins oihuiguighj iu ihuihuiohub oiuhoihukbhioh",
-          isComplete: false,
+          status: "pending",
         },
         {
           id: "2",
           description: "30 mins",
-          isComplete: false,
+          status: "pending",
         },
       ],
     },
@@ -47,12 +76,12 @@ const Home = () => {
         {
           id: "1",
           description: "30 mins",
-          isComplete: false,
+          status: "pending",
         },
         {
           id: "2",
           description: "30 mins",
-          isComplete: false,
+          status: "complete",
         },
       ],
     },
@@ -63,7 +92,7 @@ const Home = () => {
         {
           id: "1",
           description: "30 mins",
-          isComplete: false,
+          status: "incomplete",
         },
       ],
     },
@@ -74,22 +103,177 @@ const Home = () => {
         {
           id: "1",
           description: "30 mins",
-          isComplete: false,
+          status: "partially complete",
         },
         {
           id: "2",
           description: "30 mins",
-          isComplete: false,
+          status: "pending",
         },
       ],
     },
   ])
 
-  const categories = ["Project", "Jobs", "Exercises", "Learning"]
+  const changeTaskStatus = (categoryId: string, taskId: string) => {
+    const newPlanner = [...planner].map((category) => {
+      if (category.id === categoryId) {
+        category.tasks.map((task) => {
+          if (task.id === taskId) {
+            if (task.status === "pending") {
+              task.status = "complete"
+            } else if (task.status === "complete") {
+              task.status = "partially complete"
+            } else if (task.status === "partially complete") {
+              task.status = "incomplete"
+            } else {
+              task.status = "pending"
+            }
+          }
+          return task
+        })
+      }
+      return category
+    })
+    setPlanner(newPlanner)
+  }
+
+  const categories = [
+    { label: "Project", value: "1" },
+    { label: "Jobs", value: "2" },
+    { label: "Personal Learning", value: "3" },
+    { label: "Gym/Exercises", value: "4" },
+  ]
+
+  const addTask = () => {
+    if (newTaskInput.id !== "" && newTaskInput.description !== "") {
+      const newPlanner = [...planner].map((category) => {
+        if (category.id === newTaskInput.id) {
+          return {
+            ...category,
+            tasks: [
+              ...category.tasks,
+              {
+                id: "3",
+                description: newTaskInput.description,
+                status: "pending",
+              },
+            ],
+          }
+        }
+        return category
+      })
+      setPlanner(newPlanner)
+      setIsDialogOpen(false)
+    }
+  }
 
   return (
     <div className="flex min-h-screen w-full flex-col gap-5 p-6">
       <div className="flex w-full items-center justify-center gap-5">
+        <Button
+          variant="outline"
+          size="icon-xs"
+          onClick={() => {
+            date.setDate(date.getDate() - 1)
+            setDate(new Date(date))
+          }}
+        >
+          <ChevronLeftIcon />
+        </Button>
+        <h1>{date.toLocaleDateString()}</h1>
+        <Button
+          variant="outline"
+          size="icon-xs"
+          onClick={() => {
+            date.setDate(date.getDate() + 1)
+            setDate(new Date(date))
+          }}
+        >
+          <ChevronRightIcon />
+        </Button>
+      </div>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger>Add Task</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>New Task</DialogTitle>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Select
+                items={categories}
+                value={newTaskInput.id}
+                onValueChange={(e) =>
+                  setNewTaskInput({ ...newTaskInput, id: e })
+                }
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {categories.map((category) => (
+                      <SelectItem key={category.value} value={category.value}>
+                        {category.label}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </Field>
+            <Field>
+              <Label htmlFor="description">Description</Label>
+              <Input
+                id="description"
+                name="description"
+                value={newTaskInput.description}
+                onChange={(e) =>
+                  setNewTaskInput({
+                    ...newTaskInput,
+                    description: e.target.value,
+                  })
+                }
+              />
+            </Field>
+            <Button onClick={addTask}>Add</Button>
+          </FieldGroup>
+        </DialogContent>
+      </Dialog>
+      <div className="flex flex-col gap-17">
+        {planner.map((category) => (
+          <div key={category.id}>
+            <p className="mb-3 font-bold uppercase">{category.name}</p>
+            <div className="flex flex-col gap-3">
+              {category.tasks.map((task) => (
+                // <div key={task.id} className="bg-green-400 p-2">
+                //   <p>{task.description}</p>
+                // </div>
+                <Card
+                  key={task.id}
+                  className={
+                    task.status === "complete"
+                      ? "bg-green-900"
+                      : task.status === "partially complete"
+                        ? "bg-yellow-900"
+                        : task.status === "incomplete"
+                          ? "bg-red-900"
+                          : ""
+                  }
+                  // onClick={() => changeTaskStatus(category.id, task.id)}
+                  onDoubleClick={() => console.log("OKAY")}
+                  onLon
+                >
+                  <CardContent>
+                    <p>{task.description}</p>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* <div className="flex w-full items-center justify-center gap-5">
         <Button
           variant="outline"
           size="icon-xs"
@@ -133,7 +317,7 @@ const Home = () => {
             </div>
           </TabsContent>
         ))}
-      </Tabs>
+      </Tabs> */}
     </div>
   )
 }
--- changelog.md
+++ changelog.md
@@ -1,10 +1,17 @@
+### 20th August 2026
+
+- For mobile devices, tasks are displayed as a vertical list by category.
+- Clicking on a task will change its 'status' (denoted by its colour). The status of a task can be: pending (uncoloured), complete (green), partially complete (yellow) or incomplete (red).
+- Can add a new task to a category
+
+
 ### 19th August 2026
 
 - Reinitialised project to have shadcn configured from the beginning. Adding shadcn to the existing Next.js project would not work properly.
-- First making the design for smaller screens. Will have tabs for the categories, and tasks will be displayed underneath.
+- First making the design for smaller screens. Experimenting with design.
 
 ### 17th August 2026
 
-- First commit
-- Can cycle between dates
-- Can add tasks
+- First commit.
+- Can cycle between dates.
+- Can add tasks.
